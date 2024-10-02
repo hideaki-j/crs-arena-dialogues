eb0a86d1ee4fb121f3be84f2a6500aec678c5af1
hideaki-j <hj.hideaki.joko@gmail.com>
2024-10-02 10:09:52 +0200

    apply black formatter

--- tool/metrics.py
+++ tool/metrics.py
@@ -10,8 +10,8 @@ References:
 
 import random
 from nltk.tokenize import word_tokenize
-import numpy as np
 from typing import List, Tuple, Union
+
 
 def _truncate_response_set(response_set: List[str], max_token_length: int) -> List[str]:
     truncated_response_set = []
@@ -24,20 +24,25 @@ def _truncate_response_set(response_set: List[str], max_token_length: int) -> Li
         else:
             remaining_space = max_token_length - token_count
             words_to_append = words[:remaining_space]
-            truncated_response_set.append(' '.join(words_to_append))
+            truncated_response_set.append(" ".join(words_to_append))
             token_count += len(words_to_append)
             break
-    
+
     # Check if total token count is less than max_token_length
     if token_count < max_token_length:
-        print("\033[93mWarning: The response did not reach the maximum token limit.\033[0m")
+        print(
+            "\033[93mWarning: The response did not reach the maximum token limit.\033[0m"
+        )
 
     return truncated_response_set
+
 
 class DistinctNgrams:
     """Class to calculate Distinct-n metric as per Tevet's implementation."""
 
-    def __init__(self, n: int = 2, tokenizer_name: str = 'split', use_lower_case: bool = True) -> None:
+    def __init__(
+        self, n: int = 2, tokenizer_name: str = "split", use_lower_case: bool = True
+    ) -> None:
         self.n = n
         self.tokenizer_name = tokenizer_name
         self.use_lower_case = use_lower_case
@@ -52,30 +57,47 @@ class DistinctNgrams:
             list: A list of tokens from the input line.
         """
         if self.use_lower_case:
-            line = line.lower() # this is because M2M is lowercased dataset, thus for fair comparison we need to lowercase the other datasets as well.
-        if self.tokenizer_name == 'split':
-            return [e for e in line.replace('.','').replace('\n','').split(' ') if e != ''] # The same as Tevet's implementation
-        elif self.tokenizer_name == 'nltk':
+            line = (
+                line.lower()
+            )  # this is because M2M is lowercased dataset, thus for fair comparison we need to lowercase the other datasets as well.
+        if self.tokenizer_name == "split":
+            return [
+                e for e in line.replace(".", "").replace("\n", "").split(" ") if e != ""
+            ]  # The same as Tevet's implementation
+        elif self.tokenizer_name == "nltk":
             return word_tokenize(line)
         else:
-            raise ValueError("Invalid tokenizer_name. Valid options are 'split' and 'nltk'.")
+            raise ValueError(
+                "Invalid tokenizer_name. Valid options are 'split' and 'nltk'."
+            )
 
     def lines_to_ngrams(self, lines: List[str]) -> List[List[Tuple[str, ...]]]:
         ngram_lists = []
         for line in lines:
             words = self._tokenize(line)
-            ngrams = [tuple(words[i:i+self.n]) for i in range(len(words)-self.n+1)]
+            ngrams = [
+                tuple(words[i : i + self.n]) for i in range(len(words) - self.n + 1)
+            ]
             ngram_lists.append(ngrams)
         return ngram_lists
 
-    def _calculate_unique_ngram_ratio(self, ngram_lists: List[List[Tuple[str, ...]]]) -> float:
+    def _calculate_unique_ngram_ratio(
+        self, ngram_lists: List[List[Tuple[str, ...]]]
+    ) -> float:
         ngrams = [item for sublist in ngram_lists for item in sublist]  # flatten
-        return len(set(ngrams)) / len(ngrams) if len(ngrams) > 0 else 0.
+        return len(set(ngrams)) / len(ngrams) if len(ngrams) > 0 else 0.0
 
     def calculate_distinct_n(self, response_set: List[str]) -> float:
         return self._calculate_unique_ngram_ratio(self.lines_to_ngrams(response_set))
 
-    def calculate_normalized_distinct_n(self, response_set: List[str], return_avg: bool = True, max_token_length: int = 7012, iterations: int = 100, seed: int = 42) -> Union[float, List[float]]:
+    def calculate_normalized_distinct_n(
+        self,
+        response_set: List[str],
+        return_avg: bool = True,
+        max_token_length: int = 7012,
+        iterations: int = 100,
+        seed: int = 42,
+    ) -> Union[float, List[float]]:
         """Calculates the truncated Distinct-n metric for a set of responses.
 
         This method implements the truncated Distinct-n metric as described in Joko et al. (2024).
@@ -102,7 +124,9 @@ class DistinctNgrams:
         random.seed(seed)
         for _ in range(iterations):
             shuffled_response_set = random.sample(response_set, len(response_set))
-            truncated_response_set = _truncate_response_set(shuffled_response_set, max_token_length)
+            truncated_response_set = _truncate_response_set(
+                shuffled_response_set, max_token_length
+            )
             distinct_n = self.calculate_distinct_n(truncated_response_set)
             distinct_n_values.append(distinct_n)
 
